be4fb724bcad07f4ed08c8becef2e856861d4826
Sjöberg Mats <mats.sjoberg@aalto.fi>
2018-05-31 12:19:50 +0300

    Add options for setting visual feature offsets

--- src/models/s2s.h
+++ src/models/s2s.h
@@ -381,9 +381,9 @@ public:
   EncoderVisual(Ptr<Options> options) : EncoderBase(options) {
     if (picsom_data == nullptr) {
       const string fname = options_->get<std::string>("visual-feature");
-      cout << "Loading PicSOM data from [" << fname << "]" << endl;
+      LOG(info, "Loading PicSOM data from [{}]", fname);
       picsom_data = new picsom::bin_data(fname);
-      cout << picsom_data->str() << endl;
+      LOG(debug, "PicSOM bin info: {}", picsom_data->str());
     }
     vdim_ = (int)picsom_data->vdim();
   }
@@ -393,7 +393,7 @@ public:
     assert(vdim_ > 0);
     size_t offset = batch->visualFeatureOffset();
     if (offset > 0)
-      cout << "OFFSET " << offset << endl;
+      LOG(debug, "PicSOM feature OFFSET={}", offset);
     
     auto subBatch = (*batch)[batchIndex_];
     int dimBatch = subBatch->batchSize();
